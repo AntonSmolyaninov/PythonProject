9c5f076d4e47f3994f0c090b26f0a6005d2e0c08
Anton <klip07@rambler.ru>
2025-10-01 00:53:17 +0800

    chang htmlcov/

--- src/external_api.py
+++ src/external_api.py
@@ -1,25 +1,24 @@
 import os
-
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.getenv("API_KEY")
 
 
 def convert_to_rub(amount: float, currency_code: str) -> float:
     """
     Конвертировать сумму из заданной валюты в рубли по онлайн-курсу.
 
-    :param amount: сумма для конвертации (float или int)
-    :param currency_code: строковый ISO-код валюты (например, 'USD', 'EUR')
+    :param amount: сумма для конвертации
+    :param currency_code: ISO-код валюты ('USD', 'EUR' и т.д.)
     :return: сумма в рублях (float)
     :raises RuntimeError: если не найден API-ключ
     """
-    if not API_KEY:
+    api_key = os.getenv("API_KEY")
+    if not api_key:
         raise RuntimeError("Не найден ключ API_KEY в .env")
     url = "https://api.apilayer.com/exchangerates_data/convert"
     params = {"to": "RUB", "from": currency_code, "amount": str(amount)}
-    headers = {"apikey": API_KEY}
+    headers = {"apikey": api_key}
     resp = requests.get(url, params=params, headers=headers)
     return float(resp.json()["result"])
