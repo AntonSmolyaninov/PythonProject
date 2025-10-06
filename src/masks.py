521655142f73f3b37ef3d1cf9ed9ac7c2b86e32b
Anton <klip07@rambler.ru>
2025-10-06 09:19:03 +0800

    cang readme, add transactions.csv and transactions_exel.xlsx

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,22 @@
 import logging
+import os
 
+# Создание директории для логов, если она не существует
+log_directory = '../logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# Настройка логирования
 logger = logging.getLogger('masks')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/masks.log', encoding='utf-8')
+
+file_handler = logging.FileHandler(os.path.join(log_directory, 'masks.log'), encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
-
 def get_mask_card_number(card_number: str) -> str:
-    """Функция принимает на вход номер карты и возвращает ее маску.
-    Номер карты замаскирован и отображается в формате XXXX XX** **** XXXX"""
+    """Функция принимает на вход номер карты и возвращает ее маску."""
     logger.info('Проверяем номер карты')
     cleaned_number = "".join(filter(str.isdigit, card_number))
 
@@ -22,24 +28,19 @@ def get_mask_card_number(card_number: str) -> str:
     logger.info('Возвращаем маску номера')
     return mask_card_number
 
-
-if __name__ == "__main__":
-    print(get_mask_card_number("1234 5678 9012 3456"))
-
-
 def get_mask_account(account_number: str) -> str:
-    """Функция принимает на вход номер счета и возвращает его маску.
-    Номер счета замаскирован и отображается в формате **XXXX"""
+    """Функция принимает на вход номер счета и возвращает его маску."""
     logger.info('Проверяем номер счета')
     account_number = "".join(account_number.split())
 
     if len(account_number) < 20:
         logger.error('Произошла ошибка: некорректный номер')
         return "Введен некорректный номер."
+
     mask_account_number = f"**{account_number[-4:]}"
     logger.info('Возвращаем маску счета')
     return mask_account_number
 
-
 if __name__ == "__main__":
+    print(get_mask_card_number("1234 5678 9012 3456"))
     print(get_mask_account("12345678901234567890"))
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,20 @@
 import json
 import logging
+import os
 from typing import Any, Dict, List
 
+# Создание директории для логов, если она не существует
+log_directory = '../logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+# Настройка логирования
 logger = logging.getLogger('utils')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
+file_handler = logging.FileHandler(os.path.join(log_directory, 'utils.log'), encoding='utf-8')
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
-
 
 def load_transactions(file_path: str) -> List[Dict[str, Any]]:
     """
@@ -31,8 +37,6 @@ def load_transactions(file_path: str) -> List[Dict[str, Any]]:
         logger.error(f'Произошла ошибка: {error}. Вернем пустой список.')
         return []
 
-
 if __name__ == "__main__":
-
     result = load_transactions("../data/operations.json")
     print(result)
